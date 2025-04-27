3535dd01cfd98bd77a7f01f14655c2584e02639e
TalRasha <524948250@qq.com>
2025-04-27 17:26:56 +0800

    use a side nav as root page

--- src/app/ltcd/ltcd.component.ts
+++ src/app/ltcd/ltcd.component.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ArrayComponent } from './array/array.component';
+import { StringComponent } from './string/string.component';
 
 @Component({
   selector: 'app-ltcd',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
+  ],
   templateUrl: './ltcd.component.html',
   styleUrl: './ltcd.component.scss'
 })
 export class LtcdComponent {
-  // 下拉框的选项
-  options = [
-    { value: 'option1', label: '选项 1' },
-    { value: 'option2', label: '选项 2' },
-    { value: 'option3', label: '选项 3' }
+  links = [
+    { name: 'Array', component: ArrayComponent },
+    { name: 'String', component: StringComponent },
   ];
 
-  // 选中的值
-  selectedValue: string | null = null;
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef, static: false }) container!: ViewContainerRef;
 
-  // 处理选中值变化的事件
-  onValueChange(value: string) {
-    console.log('选中的值:', value);
+  loadComponent(component: any) {
+    if (this.container) {
+      this.container.clear();
+      const componentRef = this.container.createComponent(
+        this.resolver.resolveComponentFactory(component)
+      );
+    } else {
+      console.error('Container is not initialized in loadComponent');
+    }
+  }
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  ngAfterViewInit() {
+    this.loadComponent(this.links[0].component);
   }
 }
